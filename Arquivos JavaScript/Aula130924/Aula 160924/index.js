f958f6cb8aa4499b0493d034c4837ac1cdac743e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-16 11:40:43 -0300

    Add files via upload

--- Arquivos JavaScript/Aula130924/Aula 160924/index.js
+++ Arquivos JavaScript/Aula130924/Aula 160924/index.js
@@ -5,6 +5,15 @@ Pergunta2 = prompt("Hoje esta Claro?")
 Pergunta3 = prompt("Voce esta usando roupa azul?")
 let a = prompt("Qual é o valor de A")
 let b = prompt("Qual é o valor B")
+var x = Number (prompt("Qual o valor de X",x))
+var y = Number(prompt("Qual o valor de Y", y))
+const numero1 = Number(prompt("O valor da multiplicação de X é"))
+const numero2 = Number(prompt("O valor da multiplicação de Y é"))
+
+
+
+var resultado = Number(x+y)
+var resultado2 = Number(numero1*numero2)
 c=a
 a=b
 b=c
@@ -23,9 +32,14 @@ console.log("Sua Resposta é:",Pergunta2)
 console.log("Sua Resposta é:",Pergunta3)
 console.log("O valor de A é", a)
 console.log("O valor de B é", b)
+console.log("O resultado de X é", resultado)
+console.log("O valor da Multiplicação é ", resultado2)
+
+
 
 // Na tarefa 5, foi colocado as variaveis A b c, conforme o solicitado.//
 //Na Tarera 5, o valor de A passou para C, o valor de B passou para C, foi usados uma variavel de transição o valor C//
+// Desafio segue em anexo, X+Y e x*y
 
 
 
