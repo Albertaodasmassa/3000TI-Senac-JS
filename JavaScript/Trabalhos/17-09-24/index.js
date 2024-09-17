0701e9233f2859933e9b3a8f20398b42e5b75ef6
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-17 10:29:12 -0300

    Update index.js

--- JavaScript/Trabalhos/17-09-24/index.js
+++ JavaScript/Trabalhos/17-09-24/index.js
@@ -1,6 +1,8 @@
 console.log(5>1)
 console.log(1<2)
 console.log(3===3)
+console.log(2!==2)
+console.log("-------------")
 console.log(2*2)
 console.log(4*4)
 console.log(2>1<1)
@@ -9,6 +11,17 @@ console.log(3+4)
 console.log(3*5/2)
 console.log((5-4)*-1)
 console.log(234%5)
+console.log("-------------")
+const a = true
+const b = false
+const c = true
+console.log(a&&b)
+console.log(b&&c)
+console.log(a&&c)
+console.log(a&&b&&c)
+
+
+
 
 
 
