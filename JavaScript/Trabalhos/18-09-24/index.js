60133d6255026c491eca41b5dd6627d74784150e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-17 23:11:48 -0300

    Update index.js

--- JavaScript/Trabalhos/18-09-24/index.js
+++ JavaScript/Trabalhos/18-09-24/index.js
@@ -9,9 +9,16 @@ const frasemini = frase5.toLowerCase()
 const frase6 = frasemini .replaceAll("o","i")
 const frase7 = frasemini
 const numero = Number(prompt("Por favor Insira uma numero de 0 a 4"))
-const raca = ["dog","dog2","dog3","dog4","dog5"]
+const raca = [1,"dog"]
+const raca2 = [2,"dog2"]
+const raca3 = [3,"dog3"]
+const raca4 = [4,"dog4"]
+const raca5 = [5,"dog5"]
 
-const racanum = numero + raca 
+
+const racanum = numero + raca + raca2 + raca3 + raca4 + raca5
+
+
 const arrays = [1,2,3,4,5,6]
 arrays.push(7)
 const arrays2 = arrays.splice(4,5)
@@ -25,6 +32,6 @@ console.log(frase5)
 console.log(frasemini)
 console.log(frase6)
 console.log(frase7.length)
-console.log("A Raça Numero é:",racanum)
+console.log("A Raça Numero é:",raca)
 console.log(arrays.length)
 
