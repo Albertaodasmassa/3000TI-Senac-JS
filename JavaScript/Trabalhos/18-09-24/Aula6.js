e1ac3525448c1abb5bc478cf52768d4bd4870392
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-18 13:03:05 -0300

    Add files via upload

---
+++ JavaScript/Trabalhos/18-09-24/Aula6.js
@@ -0,0 +1,9 @@
+let num1 = Number(prompt("Por favor insira um numero 1"))
+let num2 = Number(prompt("Por favor insira o numero 2"))
+ 
+let comp = num1 >= num2
+let comp2 = num2 >= num1
+
+
+console.log(comp)
+console.log(comp2)
