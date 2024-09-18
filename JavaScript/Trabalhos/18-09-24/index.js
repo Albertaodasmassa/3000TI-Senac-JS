f580a2180795b3ac674a01d56ea06e64eb08ae78
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-18 09:05:18 -0300

    Update index.js

--- JavaScript/Trabalhos/18-09-24/index.js
+++ JavaScript/Trabalhos/18-09-24/index.js
@@ -8,15 +8,8 @@ const frase5 = prompt("Por favor Digite Uma frase!!")
 const frasemini = frase5.toLowerCase()
 const frase6 = frasemini .replaceAll("o","i")
 const frase7 = frasemini
-const numero = Number(prompt("Por favor Insira uma numero de 0 a 4"))
-const raca = [1,"dog"]
-const raca2 = [2,"dog2"]
-const raca3 = [3,"dog3"]
-const raca4 = [4,"dog4"]
-const raca5 = [5,"dog5"]
-
-
-const racanum = numero + raca + raca2 + raca3 + raca4 + raca5
+const dog = ["Pasto alemão","Shitzu","buldogue","husky","pitbull"]
+const raca = prompt("Digite um Numero de 0 a 4 ")
 
 
 const arrays = [1,2,3,4,5,6]
@@ -32,6 +25,5 @@ console.log(frase5)
 console.log(frasemini)
 console.log(frase6)
 console.log(frase7.length)
-console.log("A Raça Numero é:",raca)
+console.log("A Raça Numero é:"+ dog[raca])
 console.log(arrays.length)
-
