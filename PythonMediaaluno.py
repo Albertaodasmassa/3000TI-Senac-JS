043524ab18fd073a79af0f7fc4af85fea0fbed07
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-19 13:01:54 -0300

    Update PythonMediaaluno.py

--- PythonMediaaluno.py
+++ PythonMediaaluno.py
@@ -1,6 +1,6 @@
 Nome = input("Qual é seu nome?")
 Idade = input("Qual e sua Idade")
-nota1 = input(float("Por favor insira a sua nota1:"))
+nota1 = input(Float("Por favor insira a sua nota1:"))
 nota2 = input(Float("Por Favor Insira a sua nota2:"))
 nota3 = input(Float("Por Favor Insira a sua nota3:"))
 
@@ -10,4 +10,4 @@ somatotal = (nota1+nota2+nota3)/3
 
 print("Olá Seja bem vindo!",Nome)
 print("Sua idade é?",Idade)
-print("Sua nota é"somatotal)
+print("Sua nota é",somatotal)
