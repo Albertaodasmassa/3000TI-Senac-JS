94c0ab5ca1d25f21a327bbbedd9d545d5d7a391e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-19 11:11:32 -0300

    Create Exercicio4.js

---
+++ JavaScript/Trabalhos/19-09-24-02/Exercicio4.js
@@ -0,0 +1,4 @@
+let arrays = ["X","Alaminuta","Bolo de cenoura","bolo de chocolate","Pastel de Calabresa c/Queijo"]
+
+console.log(arrays)
+console.log("Essas São minhas comidas preferidas:  "+arrays)
