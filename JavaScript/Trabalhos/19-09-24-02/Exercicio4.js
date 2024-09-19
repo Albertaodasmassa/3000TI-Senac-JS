2a202a1a0e10b4bb54229ac46d9253d351be672d
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-19 11:45:38 -0300

    Update Exercicio4.js

--- JavaScript/Trabalhos/19-09-24-02/Exercicio4.js
+++ JavaScript/Trabalhos/19-09-24-02/Exercicio4.js
@@ -1,7 +1,7 @@
 let arrays = ["X","Alaminuta","Bolo de cenoura","bolo de chocolate","Pastel de Calabresa c/Queijo"]
 let array2 = ["X","Alaminuta","Bolo de cenoura","bolo de chocolate","Pastel de Calabresa c/Queijo"]
 let comidas = prompt("Qual é sua comida preferida:?")
-arrays.push(comidas)
+const novaarray = array2 .replaceAll("o","i")
 
 console.log(arrays)
 console.log("Essas São minhas comidas preferidas:  "+arrays[0])
@@ -9,6 +9,7 @@ console.log("Essas São minhas comidas preferidas:  "+arrays[1])
 console.log("Essas São minhas comidas preferidas:  "+arrays[2])
 console.log("Essas São minhas comidas preferidas:  "+arrays[3])
 console.log("Essas São minhas comidas preferidas:  "+arrays[4])
+
 
 console.log(array2)
 console.log(comidas)
