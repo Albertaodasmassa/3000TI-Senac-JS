af554220d268ea5da78ee030803997e71359d10c
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-20 19:53:38 -0300

    Update bool.py

--- Programação/Python/bool.py
+++ Programação/Python/bool.py
@@ -1,5 +1,5 @@
-media1 = float(input("Por Favor Insira a Nota 1:"))
-media2 = float(input("Por Favor Insira a Nota 2:"))
+media1 = int(input("Por Favor Insira a Nota 1:"))
+media2 = int(input("Por Favor Insira a Nota 2:"))
 media3 = float(input("Por Favor Insira a Nota 3:"))
 
 mediatotal = (media1+media2+media3)/3
@@ -9,10 +9,14 @@ media6 = mediatotal == 5
 media7 = mediatotal <= 8
 media8 = mediatotal >= 9
 
-
 print(mediatotal)
 print(media4)
 print(media5)
 print(media6)
 print(media7)
 print(media8)
+
+if mediatotal > 6:
+    print("Aprovado")
+else:
+    print("Aluno Reprovado")
