037165737f1f88fe2058ffe9e905853de85c4f39
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-19 23:01:51 -0300

    Update imprimirts.cs

--- Programação/C#/imprimirts.cs
+++ Programação/C#/imprimirts.cs
@@ -5,19 +5,19 @@ namespace Teste
     {
         static void Main()
         {
-        string = nome,sobrenome;
         int idade;
+        string nome;
+        string sobrenome;
 
-        Console.Write("Digite o Seu Nome");
+        Console.Write("Digite o Seu Nome:");
         nome=Console.ReadLine();
-        Console.Write("Digite Sua Idade");
+        Console.Write("Digite Sua Idade:");
         idade=Convert.ToInt16(Console.ReadLine());
-        Console.Write("Digite o Seu Sobrenome");
+        Console.Write("Digite o Seu Sobrenome:");
         sobrenome=Console.ReadLine();
-        juntonome = nome + sobrenome;
-        Console.WriteLine("Bem vindo ao Sistema:"+juntonome);
+        Console.WriteLine("Seu Nome é:  " + nome +  "Seu Sobrenome é:  " + sobrenome);
+        Console.WriteLine("Sua Idade em Anos:{0}",idade);
 
-        
         }
         
     }
