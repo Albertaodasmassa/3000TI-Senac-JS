1c0236bcc679784e7bdd437334e70361ac67a5ce
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-20 19:50:16 -0300

    Create bool.py

---
+++ Programação/Python/bool.py
@@ -0,0 +1,18 @@
+media1 = float(input("Por Favor Insira a Nota 1:"))
+media2 = float(input("Por Favor Insira a Nota 2:"))
+media3 = float(input("Por Favor Insira a Nota 3:"))
+
+mediatotal = (media1+media2+media3)/3
+media4 = mediatotal > 5
+media5 = mediatotal < 5
+media6 = mediatotal == 5
+media7 = mediatotal <= 8
+media8 = mediatotal >= 9
+
+
+print(mediatotal)
+print(media4)
+print(media5)
+print(media6)
+print(media7)
+print(media8)
