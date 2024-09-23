560f93ef54cc894304ac5b2d7718be2cf2aa075d
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-23 11:32:05 -0300

    Update aula4_slides.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/aula4_slides.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/aula4_slides.js
@@ -5,7 +5,15 @@ function arrays(){
     let array4 = Number(prompt("Por favor digite um numero"))
     let array5 = Number(prompt("Por favor digite um numero"))
     let mestre = []
+    let mestre4 = []
     mestre.push(array,array2,array3,array4,array5)
     console.log("O valor é e esse:",mestre)
+    let mestre2 = (mestre[4])/2
+    let mestre3 = (mestre[0])/2
+    console.log("O valor é",mestre2)
+    console.log("O valor é",mestre3)
+    mestre4.push(mestre2,mestre3)
+    console.log("O Valor da nova array  é:",mestre4)
+ 
 }
 arrays()
