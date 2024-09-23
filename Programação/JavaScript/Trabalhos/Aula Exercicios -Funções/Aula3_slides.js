4faa8ad010f9ff3c66f6698451db5302f70e97a4
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-23 10:23:09 -0300

    Update Aula3_slides.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Aula3_slides.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Aula3_slides.js
@@ -1,9 +1,9 @@
 function calculo(){
-    let numero1 = Number(prompt("Digite o numero"))
-    let numero2 = Number(prompt("Digite o numero2"))
+    let numero1 = Number(prompt("Digite o numero:"  ))
+    let numero2 = Number(prompt("Digite o numero2:" ))
     let somatotal = numero1 + numero2
     console.log(somatotal)
-    return somatotal1
+    return somatotal
     
 }
 calculo()
