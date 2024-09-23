086d4c514a4757e9cc51b9f3c30a1200f931ccb9
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-23 09:43:05 -0300

    Update Exercicio5.js

--- Programação/JavaScript/Trabalhos/19-09-24 - Exercícios Strings e Arrays JS/Exercicio5.js
+++ Programação/JavaScript/Trabalhos/19-09-24 - Exercícios Strings e Arrays JS/Exercicio5.js
@@ -2,8 +2,7 @@ let listaDeTarefas = []
 let tarefas = prompt("Por favor digite Tarefas:")
 let tarefas2 = prompt("Por favor digite Tarefas:")
 let tarefas3 = prompt("Por favor digite Tarefas:")
-let consulta = prompt("Por favor digite o numero: ")
-let excluir = prompt("Por favor Digite a Tarefa para deletar: ")
+let excluir = prompt("Por favor Digite o numero da  Tarefa para deletar: ")
 
 let recebetarefa = listaDeTarefas.push(tarefas)
 let rectaf = listaDeTarefas.push(tarefas2)
@@ -12,4 +11,4 @@ let excluir2 = listaDeTarefas.splice(excluir,1)
 
 console.log("Essas são suas Tarefas: ",listaDeTarefas)
 console.log("A tarefa selecionada é: " + listaDeTarefas)
-console.log("A Nova lista: "+excluir2)
+console.log("A Nova lista: "+listaDeTarefas)
