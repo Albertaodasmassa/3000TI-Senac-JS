fe4eeae32a6f4908416c6334297dca908e5f61e1
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-23 11:00:36 -0300

    Update aula4_slides.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/aula4_slides.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/aula4_slides.js
@@ -1,5 +1,11 @@
-function arrays(["1,2,3,4,6"]){
+function arrays(){
+    let array = number(promt("Por favor digite um numero"))
+    let array2 = number(promt("Por favor digite um numero"))
+    let arra3 = number(promt("Por favor digite um numero"))
+    let array4 = number(promt("Por favor digite um numero"))
+    let array5 = number(promt("Por favor digite um numero"))
+    let mestre = []
     array.push(7,8)
-
+    
 }
 arrays()
