9592fd02a9fd959936ab8e28ad6cee6d6c3c9399
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-23 10:20:32 -0300

    Update Exercicio4.js

--- Programação/JavaScript/Trabalhos/19-09-24 - Exercícios Strings e Arrays JS/Exercicio4.js
+++ Programação/JavaScript/Trabalhos/19-09-24 - Exercícios Strings e Arrays JS/Exercicio4.js
@@ -1,7 +1,6 @@
 let arrays = ["X","Alaminuta","Bolo de cenoura","bolo de chocolate","Pastel de Calabresa c/Queijo"]
-let array2 = ["X","Alaminuta","Bolo de cenoura","bolo de chocolate","Pastel de Calabresa c/Queijo"]
 let comidas = prompt("Qual é sua comida preferida:?")
-comidas[1] = arrays
+arrays[1] = comidas
 
 console.log(arrays)
 console.log("Essas São minhas comidas preferidas:  "+arrays[0])
@@ -9,4 +8,3 @@ console.log("Essas São minhas comidas preferidas:  "+arrays[1])
 console.log("Essas São minhas comidas preferidas:  "+arrays[2])
 console.log("Essas São minhas comidas preferidas:  "+arrays[3])
 console.log("Essas São minhas comidas preferidas:  "+arrays[4])
-console.log(comidas)
