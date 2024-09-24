a23aa5c7f88e013440f9cee732c30bb134148d69
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 10:24:09 -0300

    Update Exericio4_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exericio4_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exericio4_exercicio.js
@@ -2,6 +2,13 @@ function soma (){
     const n1 = 2
     const n2 = 3
     const result = n1+n2
+    const n3 = 3
+    const n4 = 2
     console.log("O Resultado é:"+result)
+    console.log(n3=>n4)
+    console.log(n3>n4)
+   
 }
+soma()
+soma()
 soma()
