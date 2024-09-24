fd70563198f14b4536cb0e4528adc4eb8cbc4303
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 09:16:45 -0300

    Update Exercicio2_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio2_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio2_exercicio.js
@@ -1,1 +1,14 @@
+let textoDoUsuario = prompt("Insira um texto");
 
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+//A opção tolowercase deixa o texto em caixabaixa("Minusculo").
+//Será solicitado ao usuario que digite um texto, pois a um promt.
+//O Texto será impresso.
+//A o comando return, que retorna, o texto.
+//será uma comparação, se o usuario digitar cenoura retorna true se não retorna falso.
