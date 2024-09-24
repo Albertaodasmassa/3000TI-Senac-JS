f85ba4d45a053963a39d8a2661ba8032b0fc08ad
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 09:19:15 -0300

    Update Exercicio2_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio2_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio2_exercicio.js
@@ -12,3 +12,4 @@ console.log(resposta)
 //O Texto será impresso.
 //A o comando return, que retorna, o texto.
 //será uma comparação, se o usuario digitar cenoura retorna true se não retorna falso.
+// Ao inserir a frase Eu gosto de cenoura, o valor retorna true pois a cenoura no texto,CENOURA é bom pra vista tambem retorna true,Cenouras crescem na terra tambem retorna true. 
