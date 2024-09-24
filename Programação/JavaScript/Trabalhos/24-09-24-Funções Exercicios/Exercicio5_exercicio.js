2a63ff7e39b0d4645e33076d08146b71a64b5bf7
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 09:40:12 -0300

    Update Exercicio5_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
@@ -1,8 +1,1 @@
-function nome (){
-    const nome = prompt("Qual é seu Nome: ")
-    const idade = Number(prompt("Digite sua idade:"  ))
-    const cidade = prompt("Qual é e sua cidade:  ")
-    const info = prompt("Qual sua profissão: ")
-    console.log("Meu Nome é",+nome ,"e tenho" ,+idade, "sou da cidade" +"cidade e sou" +info)
-}
-nome()
+
