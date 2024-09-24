75c856a1879f924f4e630a8d5c345aba5d718f1a
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 19:21:51 -0300

    Update Exercicio2_Exercicio.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio2_Exercicio.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio2_Exercicio.js
@@ -1,1 +1,16 @@
+const cachorro = {
+	nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+}
 
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+// O codigo vai imprir a informação do objeto cachorro no console em forma de array.
+// O console gato vai imprir cachorro, pois no parentes esta sendo passado informações do cachorro.
+// Na variavel tartaruga esta sendo passado as informações do objeto cachorro e removido a e o pois o comando replace remove o a e o.
