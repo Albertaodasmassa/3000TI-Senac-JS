6ba6204921ecad5f9f88f3f1a15a732a5b9d985b
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 12:00:26 -0300

    Update Exericio5_slide.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Exericio5_slide.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Exericio5_slide.js
@@ -8,4 +8,10 @@ nome("Pedro")
 nome("Antonio")
 
 
-
+ const calculo = (numero1,numero2)=>{
+    const somatotal = numero1 + numero2
+    return somatotal
+    
+}
+const somatotal2 = calculo(2,2)
+console.log(somatotal2)
