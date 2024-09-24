53f378f73413dbc7c1259ddc75f7b1c466331ffa
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 19:39:20 -0300

    Create Exercicio4_Slide.js

---
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome : "Pedro",
+    idade : 20,
+    generomus : "Rock",
+}
+function pessoas2(comidas){
+infopessoa = pessoa
+
+}
+
+
+console.log("O nome da pessoa é",pessoa.nome,"ela tem",pessoa.idade,"anos e gosta muito de",pessoa.generomus)
