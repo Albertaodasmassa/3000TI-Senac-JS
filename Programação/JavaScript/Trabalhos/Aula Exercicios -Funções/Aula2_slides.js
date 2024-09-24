d89e642357a66f38ef24cfe81bb8e5eecb5acb00
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 09:51:25 -0300

    Update Aula2_slides.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Aula2_slides.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Aula2_slides.js
@@ -1,16 +1,11 @@
-let nome = function(){
-   nome = prompt("Por favor digite o seu nome: ")
+function nome(){
+   let nome = prompt("Por favor digite o seu nome: ")
     console.log(`Seu nome é ${nome}`)
     
     
 }
-nome()
-let calculo = ()=>{
-    let numero1 = Number(prompt("Digite o numero:"  ))
-    let numero2 = Number(prompt("Digite o numero2:" ))
-    let somatotal = numero1 + numero2
-    console.log(somatotal)
-    return somatotal
-    
-}
-let somatotal2 = calculo()
+nome(nome.nome)
+nome(nome.nome)
+nome(nome.nome)
+
+
