24a59b6652d15880584245e1848990fe79695c1f
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 11:55:48 -0300

    Update Exericio5_slide.js

--- Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Exericio5_slide.js
+++ Programação/JavaScript/Trabalhos/Aula Exercicios -Funções/Exericio5_slide.js
@@ -1,16 +1,1 @@
-let nome = function(){
-   nome = prompt("Por favor digite o seu nome: ")
-    console.log(`Seu nome é ${nome}`)
-    
-    
-}
-nome()
-let calculo = ()=>{
-    let numero1 = Number(prompt("Digite o numero:"  ))
-    let numero2 = Number(prompt("Digite o numero2:" ))
-    let somatotal = numero1 + numero2
-    console.log(somatotal)
-    return somatotal
-    
-}
-let somatotal2 = calculo()
+
