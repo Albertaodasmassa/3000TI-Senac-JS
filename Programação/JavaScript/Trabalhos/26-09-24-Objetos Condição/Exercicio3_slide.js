266cf4e939aa1b427c81e725b64d9a5333503d9f
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 10:29:30 -0300

    Update Exercicio3_slide.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio3_slide.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio3_slide.js
@@ -15,5 +15,9 @@ console.log(filme["visto"])
 filme.personagem = "Morphi,Kooper"
 console.log(filme.elenco+filme.personagem)
 filme.elenco[0]="xuxa"
-console.log(filme.elenco)
+console.log(filme.nome)
+console.log(filme.direcao)
+console.log(filme["lancamento"])
+console.log(filme["elenco"])
+console.log(filme["visto"])
 
