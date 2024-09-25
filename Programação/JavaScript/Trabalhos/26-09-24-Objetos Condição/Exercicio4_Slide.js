40430fdb8f2948704db3afed4e5d324149e9516f
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 11:29:02 -0300

    Update Exercicio4_Slide.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
@@ -12,9 +12,9 @@ const pessoa2 = {
    comidas:["X","pizza"],
 amigo:{
        nome:'joao',
-       idade:'20',
+       idade:20,
    }
 }
 
-console.log("O nome da pessoa é",pessoa.nome,"ela tem",pessoa.idade,"anos e gosta muito de",pessoa.generomus)
-console.log("O nome da pessoa é",pessoa2.nome,"e suas comidas preferidas são",pessoa2.comidas,"e","Seu melhor amigo se chama",pessoa2.amigo.nome,"e tem",pessoa2.amigo.idade)
+
+console.log("O nome da pessoa é",pessoa2.nome,"e suas comidas preferidas são",pessoa2.comidas,"e","Seu melhor amigo se chama",pessoa2.amigo.nome,"e tem Anos",pessoa2.amigo.idade)
