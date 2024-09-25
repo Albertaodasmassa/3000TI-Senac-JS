1c6a3afc4be78e484881b5c83fdbdce7a3396116
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 09:40:12 -0300

    Update Exercicio5_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
@@ -1,10 +1,12 @@
-function operacao(n1,n2){
-     n1 = Number(prompt("Por favor digite um numero: "))
-     n2 = Number(prompt("Por favor digite um numero: "))
-    res = n1+n2
-    return res 
+let n1 = Number(prompt("Por favor digite um numero: "))
+let n2 = Number(prompt("Por favor digite um numero: "))
+
+
+function soma(n1,n2){
+res = n1+n2
+return res 
 }
-let res2 = operacao(n1,n2)
+let res2 = soma(n1,n2)
 console.log("O Resultado é:",res2)
 
 
