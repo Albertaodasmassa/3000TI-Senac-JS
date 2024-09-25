61760b152f2bd874d1376731e31351da36188a27
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 10:31:25 -0300

    Update Exercicio4_Slide.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
@@ -7,6 +7,17 @@ function pessoas2(comidas){
 infopessoa = pessoa
 
 }
+const pessoa2 = {
+    cont cpnome = [...nome]
+    cont cpidade = [...idade]
+}
+function pessoas2(comidas){
+infopessoa = pessoa
+
+}
+
+
+
 
 
 console.log("O nome da pessoa é",pessoa.nome,"ela tem",pessoa.idade,"anos e gosta muito de",pessoa.generomus)
