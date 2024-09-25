28b8c7d48723130556898fa1b57a69deee11cf3b
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 10:49:38 -0300

    Update Exercicio4_Slide.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicio4_Slide.js
@@ -9,21 +9,16 @@ infopessoa = pessoa
 }
 const pessoa2 = {
     ...pessoa,
-    const cpnome = [...nome]
-    const cpidade = [...idade]
-    const cpgeneromus =[...generomus]
+   comidas:["X","pizza"]
+   melhoramigo:{
+       nome:'joao'
+       idade:'20'
+   }
 
 }
-function pessoas3(comidas){
-infopessoa = pessoa
 
-}
-const amigo3 ={
-    nome : "Joao",
-    idade : 23,
-}
 function pessoa4(comidas2){
-infopessoa2=amigo3
+infopessoa2=amigo
 }
 
 
