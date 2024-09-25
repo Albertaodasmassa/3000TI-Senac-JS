f1e346c71e98ce59f790c62f1f7b4f33de544e0f
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 09:32:08 -0300

    Update Exercicio5_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
@@ -1,10 +1,11 @@
 function operacao(n1,n2){
-    let n1 = Number(prompt("Por favor digite um numero: "))
-    let n2 = Number(prompt("Por favor digite um numero: "))
+     n1 = Number(prompt("Por favor digite um numero: "))
+     n2 = Number(prompt("Por favor digite um numero: "))
     res = n1+n2
     return res 
 }
-console.log("O Resultado é:",(Operacao(n1,n2)
-operacao(n1,n2)
+let res2 = operacao(n1,n2)
+console.log("O Resultado é:",res2)
+
 
 
