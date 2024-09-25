3a6343c8a29300ed705ac78d7b72a384b471dc59
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-25 09:29:59 -0300

    Update Exercicio5_exercicio.js

--- Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
+++ Programação/JavaScript/Trabalhos/24-09-24-Funções Exercicios/Exercicio5_exercicio.js
@@ -1,20 +1,10 @@
-function operacao(){
+function operacao(n1,n2){
     let n1 = Number(prompt("Por favor digite um numero: "))
     let n2 = Number(prompt("Por favor digite um numero: "))
-    let soma = n1 +n2
-    let div = n1/n2
-    let mult = n1*n2
-    let sub = n1-n2
-    let numero = (`Os numeros digitais são:${n1} e ${n2}`)
-    console.log(numero)
-    console.log("Soma: "+soma)
-    console.log("Divisão: "+div)
-    console.log("Multiplicacao: "+mult)
-    console.log("Subtração:",sub)
-    
+    res = n1+n2
+    return res 
 }
-operacao(operacao.soma)
-operacao(operacao.div)
-operacao(operacao.mult)
-operacao(operacao.sub)
-operacao(operacao.numero)
+console.log("O Resultado é:",(Operacao(n1,n2)
+operacao(n1,n2)
+
+
