32fd7a39440cb222b1166df3366b0571102a9508
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 11:23:41 -0300

    Update Exercicio1_Slides.js

--- Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio1_Slides.js
+++ Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio1_Slides.js
@@ -1,1 +1,7 @@
-let n1 = prompt
+let num1 = prompt("Favor informar o numero:")
+let num2 = prompt("Favor informar o numero:")
+function dif(num1,num2){
+    if(num1===num2)
+    console.log("Sucesso")
+}
+dif()
