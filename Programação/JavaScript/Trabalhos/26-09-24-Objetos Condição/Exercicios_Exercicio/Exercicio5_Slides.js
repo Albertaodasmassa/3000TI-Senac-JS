612594e00f06b2c3273a6adc798e96d9a2915eb2
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 10:15:07 -0300

    Update Exercicio5_Slides.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio5_Slides.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio5_Slides.js
@@ -1,10 +1,16 @@
 const pessoa={
-    nome:'Pedro'
+    nome:"Pedro",
     idade: 20,
-    profissão: 'Minerador',
+    profissão: "Minerador"
+}
+const pessoa2={
+    nome:"Alan",
+    idade: 25,
+    profissão: "Vendedor"
 }
 function infopessoa(){
-    const resultado = []
-    const info = pessoa
-    
+    const res = [pessoa.nome,pessoa.nome.length,pessoa.idade]
+    return res
 }
+
+console.log(infopessoa(pessoa))
