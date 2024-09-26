d5827d722c2e98f203382159cfc57c054e85ac4e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 11:26:16 -0300

    Create Exercicio2_Slides.js

---
+++ Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio2_Slides.js
@@ -0,0 +1,12 @@
+
+
+let num1 = prompt("Favor informar o numero:")
+let num2 = prompt("Favor informar o numero:")
+function dif(num1,num2){
+    if(num1===num2)
+    console.log("Sucesso")
+    else{
+   Console.log("O valor não e igual")
+    }
+}
+dif()
