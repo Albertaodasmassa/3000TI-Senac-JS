81f8ee854175fe445f802d30698dda1c0d72fc32
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 10:35:08 -0300

    Create exercicio6_exercicio.js

---
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/exercicio6_exercicio.js
@@ -0,0 +1,25 @@
+const carrinho =[{},{},{}]
+const sacolao={
+    nome:"banana",
+    disponibilidade:true,
+    
+}
+const sacolao2={
+    nome:"batana",
+    disponibilidade:true,
+    
+}
+const sacolao3={
+    nome:"milho",
+    disponibilidade:true,
+    
+}
+function receb(sacolao,sacolao2,sacolao3){
+   carrinho.push[0]=nome.sacolao
+    res =[sacolao.nome,sacolao.disponibilidade]
+    res2=[sacolao2.nome,sacolao2.disponibilidade]
+    res3=[sacolao3.nome,sacolao3.disponibilidade]
+}
+
+console.log(receb(sacolao.nome))
+console.log()
