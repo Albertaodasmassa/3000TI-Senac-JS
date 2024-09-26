aade5662a0ee50fc6250711a84929cf1db8ef339
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 09:14:04 -0300

    Update Exercicio4_Exercicio.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio4_Exercicio.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio4_Exercicio.js
@@ -1,4 +1,5 @@
 function pessoa2 (pessoa){
+    const infopessoa = pessoa
 }
 
 
@@ -6,5 +7,4 @@ const pessoa = {
    nome: "Sid", 
    apelidos: ["Sidão", "Albertão", "Sidoso"]
 }
-
 console.log("Eu sou", pessoa.nome, "mas pode me chamar de:",pessoa.apelidos[0],pessoa.apelidos[1],pessoa.apelidos[2])
