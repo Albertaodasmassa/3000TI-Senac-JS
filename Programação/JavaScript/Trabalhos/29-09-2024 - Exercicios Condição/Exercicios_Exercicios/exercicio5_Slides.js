09325eb21ee48cf2aa6c7d8040546b4283f0e4e3
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-27 11:36:11 -0300

    Update exercicio5_Slides.js

--- Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/exercicio5_Slides.js
+++ Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/exercicio5_Slides.js
@@ -2,15 +2,8 @@ let pessoa = prompt("Voce concluiu o ensino medio: ")
 let pessoa2 = prompt("Quantos anos voce tem?: ")
 let pessoa3 = prompt("Voce cursa outra faculdade?:")
 
-if(!pessoa){
-    console.log("Voce ainda não completou o ensino medio:")
-}
-if(!pessoa3){
-    console.log("Voce ainda não completou o ensino medio:")
-}
-if(!pessoa2){
-    console.log("Voce ainda não completou 18 anos, e não pode ingressar na faculdade.")
-}
-if(pessoa2<18){
-    console.log("Voce ainda não completou 18 anos!")
+if(pessoa==="sim"&&pessoa>=18&&pessoa3==="nao"){
+    console.log("Voce Pode estudar!")
+}else{
+console.log("Voce não pode estudar na faculdade ainda!")
 }
