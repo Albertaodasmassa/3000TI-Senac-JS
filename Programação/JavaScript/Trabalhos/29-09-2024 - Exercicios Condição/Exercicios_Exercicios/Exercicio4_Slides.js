9061e83192bbd9d979ffa8746535917c4af29ac8
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-27 09:38:50 -0300

    Create Exercicio4_Slides.js

---
+++ Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio4_Slides.js
@@ -0,0 +1,9 @@
+const pok = prompt("Por Favor Selecione os seus Pokémon:")
+switch(pok){
+    case 'Planta e Veneno':
+    console.log('Bulbasauro')
+    case 'Fogo'
+    console.log('Charmander')
+    case 'Agua'
+    console.log('Squirtle')
+}
