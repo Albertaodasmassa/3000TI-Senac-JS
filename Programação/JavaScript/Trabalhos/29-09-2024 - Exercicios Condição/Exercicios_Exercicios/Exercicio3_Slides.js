b64eb79f591b371277d59c4080916b5cbd555691
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-27 09:23:34 -0300

    Update Exercicio3_Slides.js

--- Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio3_Slides.js
+++ Programação/JavaScript/Trabalhos/29-09-2024 - Exercicios Condição/Exercicios_Exercicios/Exercicio3_Slides.js
@@ -1,1 +1,12 @@
-
+function dif(num1,num2){
+    if(num1>num2){
+    console.log("O Numero digitado e Maior: ")
+    }else if(num1<num2) {
+   console.log("O Numero e Menor que:")
+    }else {
+    console.log("Os Numeros são iguais")
+}
+}
+let num1 = prompt("Favor informar o numero:")
+let num2 = prompt("Favor informar o numero:")
+dif(num1,num2)
