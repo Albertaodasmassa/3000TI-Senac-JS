45fb6ee9beaa188bcc11dcfc526f14e1f769a794
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-27 10:09:46 -0300

    Update Exercicio1_Exercicio.js

--- Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio1_Exercicio.js
+++ Programação/JavaScript/Trabalhos/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio1_Exercicio.js
@@ -16,5 +16,7 @@ console.log(filme.elenco[0])
 console.log(filme.elenco[filme.elenco.length - 1])
 console.log(filme.transmissoesHoje[2])
 //vai ser impresso na posição 0 que o começo do array elenco, será impresso Matheus Nachtergaele.
-//No array elenco vai ser diminuido um caracter.
+//No array elenco vai ser diminuido um numero.
 //No ultimo será impresso o filme na posição 2 canal: "Globo", horario: "14h".
+//O que vai ser impresso no console? No console vai ser impresso o objeto filme .elenco na posição 0,  console.log(filme.transmissoesHoje[2]) será acessado o valor posição 2 do array transmissoesHoje que será canal globo horario 14horas.
+
