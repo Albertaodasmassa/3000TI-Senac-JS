6ad0833bff4dce87347700123aef6714d745e82c
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-29 11:00:30 -0300

    Create Exercicio_Exericio6.js

---
+++ Programação/JavaScript/Trabalhos/Exercicios Condição/Exercicios_Slides/Exercicio_Exercicio/Exercicio_Exericio6.js
@@ -0,0 +1,17 @@
+let turno  = prompt("Por favor digite O seu turno M/V/N: ")
+
+case(turno){
+    case 'M':
+    console.log('Bom dia ,Matutino')
+    break
+    case 'N':
+    console.log('Bulbasauro')
+    break
+    case 'V':
+    console.log('Bulbasauro')
+    break
+    default:
+    console.log('Turno Não encontrado!')
+    break
+    
+}
