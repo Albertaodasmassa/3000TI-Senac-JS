68067732d18c0167f7dce3a5209075ba7ee72fba
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-30 11:43:04 -0300

    Update exercicio_slides1.js

--- Programação/JavaScript/Trabalhos/Aula de Laços/exercicio_slides1.js
+++ Programação/JavaScript/Trabalhos/Aula de Laços/exercicio_slides1.js
@@ -1,1 +1,12 @@
+let n1 = Number(prompt("Digite um Numero"))
+let n2 = Number(prompt("Digite um Numero"))
+let n3 = Number(prompt("Digite um Numero"))
+let n4 = Number(prompt("Digite um Numero"))
+let n5 = Number(prompt("Digite um Numero"))
 
+let soma = n1+n2+n3+n4+n5
+
+while(soma!==0){
+    console.log("A soma total é:");
+    soma = n1+n2+n3+n4+n5
+}
