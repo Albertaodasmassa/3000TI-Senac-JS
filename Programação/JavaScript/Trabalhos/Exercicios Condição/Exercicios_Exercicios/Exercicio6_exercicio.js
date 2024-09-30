68f9a222f5f8bc9d93475782cc7604323e7e3d41
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-30 10:18:02 -0300

    Update Exercicio6_exercicio.js

--- Programação/JavaScript/Trabalhos/Exercicios Condição/Exercicios_Exercicios/Exercicio6_exercicio.js
+++ Programação/JavaScript/Trabalhos/Exercicios Condição/Exercicios_Exercicios/Exercicio6_exercicio.js
@@ -1,8 +1,7 @@
-const filme = prompt("Qual genero de filme quer assistir:")
-const filmegen = prompt("Qual genero de filme quer assistir:")
-const ing = 15
+const filme = prompt("Qual genero de filme quer assistir: ")
+const ing = prompt("Por favor insira o valor do ingresso: ")
 
-if(filme==="fantasia"&&ing>15){
+if(filme==="fantasia"&&ing<15){
     console.log("Bom filme!")
 }else{
 console.log("Escolha outro filme")
