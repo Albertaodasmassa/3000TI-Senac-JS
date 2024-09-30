d2ba4268b439ea4b46e3f3dbe5e2835192e33b7e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-30 09:21:10 -0300

    Create Exercicio6_exercicio.js

---
+++ Programação/JavaScript/Trabalhos/Exercicios Condição/Exercicios_Exercicios/Exercicio6_exercicio.js
@@ -0,0 +1,10 @@
+const filme = prompt("Qual genero de filme quer assistir:")
+const filmegen = "fantasia"
+const ing = 15
+
+if(filme===filmegen){
+    console.log("Bom filme!")
+}else if(ing < 15){
+   console.log("Bom filme") 
+}else
+console.log("Escolha outro filme!!")
