88785c3d2d3eba851bd67b675f9374677a0c7448
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-01 11:49:20 -0300

    Update Exercicio_Exercicio4.js

--- Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio4.js
+++ Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio4.js
@@ -2,9 +2,10 @@ const bixo = prompt("Quantos bixos de estimação voce tem?")
 const array = [""]
 
 
-for(let i=0; i++){
+for(let i=0; bixo>0; i++){
       let userbixo = bixo
       if(bixo>0){
        console.log(" Que pena! Você pode adotar um pet")
+       break
       }
    }
