2a1dbb5638355f887f2a316743e4116738d6b42c
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-01 11:21:58 -0300

    Update Exercicio_Exercicio2.js

--- Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio2.js
+++ Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio2.js
@@ -4,3 +4,5 @@ for (let numero of lista) {
 		console.log(numero)
 	}
 }
+//1)O que vai ser impresso no console?19,21,23,25,27,30
+//2)Se eu quisesse acessar o índice de cada elemento dessa lista, o for...of... é suficiente? Se sim, o que poderia ser usado para fazer isso?Não somente o for of não e o suficiente, pois será necessario colocar a posição do array.
