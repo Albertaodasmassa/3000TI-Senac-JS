34b950941f141cfcfc2015718a306c7b6d554a60
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-02 09:15:19 -0300

    Update Exercicio_Exercicio4.js

--- Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio4.js
+++ Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio4.js
@@ -1,15 +1,14 @@
-let bixo = prompt("Quantos bixos de estimação voce tem?")
+let bixo = Number(prompt("Quantos bixos de estimação voce tem?"))
 let array = [""]
-let nome = prompt("Qual e seu nome")
 
 if(bixo===0){
 console.log("Que pena! Você pode adotar um pet!")
 } else{
 for(let i =0; i<=bixo; i++){
      nome = prompt("Qual o nome do bixo de pelucia")
-     nome = prompt("Qual o nome do bixo de pelucia")
-     nome = prompt("Qual o nome do bixo de pelucia")
-     nome = array
+     array.push(nome)
 }
 }
-console.log("O nome dos bixos de pelucia são",array)
+
+
+
