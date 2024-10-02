1abeab07535587db0b996538a09c5f8eeb139d7b
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-02 10:15:56 -0300

    Update Exercicio_Exercicio5.js

--- Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio5.js
+++ Programação/JavaScript/Trabalhos/Aula de Laços/Exercicios sobre Laços_exercicios/Exercicio_Exercicio5.js
@@ -1,6 +1,15 @@
 let arrayog = [1,2,4,5,6,7,8]
+let maiorN = 0
 
-function(arrayog){
-for(let i =0; i=>arrayog; i++)
+function tetra(arrayog){
+for(let i = 0; i < arrayog.length; i++){
 }
+console.log(arrayog)
+}
+tetra(arrayog[0])
+tetra(arrayog[1])
+tetra(arrayog[2])
+tetra(arrayog[3])
+tetra(arrayog[4])
+
 
