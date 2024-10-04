42c0d91160b6df6dd16f4dd3144afa2fe4fb8396
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-04 10:24:01 -0300

    Update trabalho_final.js

--- Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/trabalho_final.js
+++ Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/trabalho_final.js
@@ -1,16 +1,20 @@
-let cidade = prompt("Selecione uma opcao/ cadastrar Abrigo /listar abrigo/Procurar Abrigo: ")
+let cidade = prompt("Selecione uma opcao/ cadastrar abrigo /listar abrigo/Procurar Abrigo: ")
 function cadfun(cidade){
-    nomecid = ("Qual e o nome da cidade")
+    console.log("======================================")
+    let nomecid = prompt("Qual e o nome da cidade que gostaria de cadastrar ?")
+    let enderecocid = prompt("Qual e endereco da cidade que gostaria de cadastrar? ")
+    console.log("Cidade Cadastrada com sucesso:",nomecid)
+    console.log("Endereço cadastrado com sucesso:",enderecocid)
+    console.log("====================================")
 }
-
-
 switch(cidade){
-  case 'cadastrar Abrigo':
+  case 'cadastrar abrigo':
   console.log('Gostaria de cadastrar um Novo Abrigo?')
   cadfun(cidade)
   break
   case 'listar Abrigo':
   console.log('Gostaria de Listar os abrigo existentes?')
+  abri(cidade)
   break
   case 'procurar':
   console.log('Gostaria de Procurar os abrigos existentes')
@@ -21,9 +25,13 @@ switch(cidade){
 }
 
 
+const abrigos ={
+nomeabg : [""]
+}
 
-
-
-
+function abri(abrigo){
+    const resabrigo = [abrigos.nomeabg]
+}
+console.log(abri(abrigos))
 
 
