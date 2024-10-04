a5eb4f6c28c40a382a8271753774760dfcff2ffa
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-04 17:03:58 -0300

    Update trabalho_final.js

--- Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/trabalho_final.js
+++ Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/trabalho_final.js
@@ -1,39 +1,37 @@
-let cidade = prompt("Selecione uma opcao|cadastrar abrigo |listar abrigo|Procurar Abrigo|: ")
-let res = prompt("Gostaria de cadastrar uma nova cidade? Sim|Não")
+let cidade = prompt("|cadastrar abrigo |listar abrigo|Procurar Abrigo|: ")
+let res = prompt("Gostaria de cadastrar um novo abrigo? Sim|Não")
 function cadfun(cidade){
     console.log("======================================")
     let nomecid = prompt("Qual e o nome da cidade que gostaria de cadastrar ?")
     let enderecocid = prompt("Qual e endereco da cidade que gostaria de cadastrar? ")
     let telefone = Number(prompt("Qual e o telefone do abrigo?"))
-    let lotacao = Number(prompt("Qual é a lotacao do abrigo?"))
-    console.log("Cidade Cadastrada com sucesso:")
-    console.log("Endereço cadastrado com sucesso:")
-    console.log("Telefone cadastrado com sucesso:")
-    console.log("Informação cadastrada com sucesso:")
+    let lotacao = Number(prompt("Qual é a lotacao do abrigo em pessoas?"))
+    console.log("======================================")
+    console.log("Cidade Cadastrada com sucesso.")
+    console.log("Endereço cadastrado com sucesso.")
+    console.log("Telefone cadastrado com sucesso.")
+    console.log("Informação cadastrada com sucesso.")
     console.log("====================================")
 }
-    function abri(abrigos){
+    function listabrigo(infoabrigo,cadfun){
     console.log=("=======Bem vindo a lista de Abrigos=======")
     infoabrigo={
-        nomeabrigo:cadfun(nomecid)
-        cidadeabrigo:cadfun(enderecocid)
-        Endereçoabrigo:cadfun(telefone)
+        ...cadfun
+        nomeabrigo:cadfun(nomecid),
+        cidadeabrigo:cadfun(enderecocid),
+        Endereçoabrigo:cadfun(telefone),
         telefoneabrigo:cadfun(lotacao)
     }
 }
-
-
-
 switch(cidade){
   case 'cadastrar abrigo':
-  console.log('Gostaria de cadastrar um Novo Abrigo?')
   cadfun(cidade)
   break
   case 'listar abrigo':
   console.log('Gostaria de Listar os abrigo existentes?')
-  if(res=="sim")
+  if(res==="sim")
       console.log("=====Bem vindo ao sistema de cadastro=====")
-   abri(abrigos)
+   listabrigo(res)
   break
   case 'procurar':
   console.log('Gostaria de Procurar os abrigos existentes')
