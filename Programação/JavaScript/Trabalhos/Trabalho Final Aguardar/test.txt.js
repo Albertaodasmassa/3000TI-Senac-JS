b549eeda4d46f70a1072da02b58a9daf0e0283aa
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-04 09:06:06 -0300

    Update test.txt.js

--- Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/test.txt.js
+++ Programação/JavaScript/Trabalhos/Trabalho Final Aguardar/test.txt.js
@@ -1,10 +1,46 @@
-let abrigo prompt("Selecione os Abrigos cadastrados")
+let cidade = prompt("Selecione a cidade para cadastrar o Abrigo")
 
-switch(abrigo){
+switch(cidade){
+  case '1':
+  console.log('Sao Leopoldo')
+  break
+  case '2':
+  console.log('Sapiranga')
+  break
+  case '3':
+  console.log('Canoas')
+  break
+  case '4':
+  console.log('Porto Algre')
+  break
+  case '5':
+  console.log('Viamao')
+  break
+  default:
+  console.log('Cidade não cadastrada: Gostaria de cadastrar?')
+  break
+}
 
-
-a
-
-
+ let abrigocad={
+  nomeabri: prompt("Qual e o nome do abrigo? "),
+  cidade:  prompt("Qual e o nome da cidade do abrigo? "),
+  numero: prompt("Qual e o numero do abrigo?  "),
+  endereco: prompt("Qual e o endereço da cidade?  ")
 
 }
+console.log("Novo abrigo Cadastrado com sucesso: ",abrigocad.nomeabri)
+console.log("Nova Cidade cadastrada com sucesso",abrigocad.cidade)
+console.log("Novo do abrigo cadastrado",abrigocad.numero)
+console.log("Novo endeço de abrigo cadastrado com sucesso: ",abrigocad.endereco)
+
+let abigoinfo{
+    ...abr
+}
+
+console.log(`No abrigo numero${numero}`)
+
+
+
+
+
+
